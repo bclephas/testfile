530c62c9b8ed682ab96758504092dc5e7c75a62e
Bart Clephas <bclephas@gmail.com>
2018-01-12 23:34:58 +0100

    Initial commit of Testfile
    Requires a locally availably PyYAML (>= 3.12) copy

---
+++ testfile.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+import os
+import subprocess
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'pyyaml/lib/')))
+#sys.path.insert(0, "pyyaml/lib/")
+import yaml
+
+def _build_commandline(commands):
+    '''Commands can be a list or a simple string'''
+    if not isinstance(commands, basestring):
+        script = ';'.join(commands)
+    else:
+        script = commands
+
+    return script
+
+def _execute_commandline(script):
+    '''Executes the script provided.
+       Returns a tuple with stdout, stderr and the process's exitcode'''
+    process = subprocess.Popen(script,
+                               shell=True,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+
+    (out, err) = process.communicate()
+    
+    return (out.strip(), err.strip(), process.returncode)
+
+def _print_verbose(verbose, out, err, returncode):
+    if verbose:
+        print('Out: [\n' + out + '\n]\nErr: [\n' + err + '\n]\nReturncode: ' + str(returncode))
+
+def _print_result(test, returncode):
+    class ANSI(object):
+        ESCAPE  = '\033[%sm'
+        RESET   = ESCAPE %  '0'
+        BLACK   = ESCAPE % '30'
+        RED     = ESCAPE % '31'
+        GREEN   = ESCAPE % '32'
+        YELLOW  = ESCAPE % '33'
+        BLUE    = ESCAPE % '34'
+        MAGENTA = ESCAPE % '35'
+        CYAN    = ESCAPE % '36'
+        WHITE   = ESCAPE % '37'
+
+        @staticmethod
+        def decorate(fmt, msg):
+            return fmt + msg + ANSI.RESET
+
+    result = ANSI.decorate(ANSI.GREEN, 'PASS') if returncode == 0 else ANSI.decorate(ANSI.RED, 'FAILED')
+    print('{} ... {}'.format(test, result))
+
+def _terminate_if_required(returncode):
+    if returncode != 0:
+        sys.exit(returncode)
+
+def execute_testfile(config, verbose=False):
+    '''Executes all commands in the provided Testfile. Config is a yamlified dictionary'''
+    fixture_config = config.get('fixture', {})
+
+    # handle fixture's one time setup
+    if 'onetime_setup' in fixture_config:
+        script = _build_commandline(fixture_config['onetime_setup'])
+        (out, err, returncode) = _execute_commandline(script)
+        _print_verbose(verbose, out, err, returncode)
+        _terminate_if_required(returncode)
+
+    tests = config.get('tests', [])
+    for test in tests:
+        if 'description' in test:
+            test_description = "{} ({})".format(test['test'], test['description'])
+        else:
+            test_description = test['test']
+
+        try:
+            returncode = 0
+
+            # handle fixture's setup
+            if 'setup' in fixture_config:
+                test_setup_script = _build_commandline(fixture_config['setup'])
+                (out, err, returncode) = _execute_commandline(test_setup_script)
+                _print_verbose(verbose, out, err, returncode)
+
+            if returncode != 0:
+                continue
+
+            # handle test steps
+            test_script = _build_commandline(test['steps'])
+            (out, err, returncode) = _execute_commandline(test_script)
+            _print_verbose(verbose, out, err, returncode)
+
+            if returncode != 0:
+                continue
+        finally:
+            test_returncode = returncode
+
+            # handle fixture's teardown
+            if 'teardown' in fixture_config:
+                test_setup_script = _build_commandline(fixture_config['teardown'])
+                (out, err, returncode) = _execute_commandline(test_setup_script)
+                _print_verbose(verbose, out, err, returncode)
+   
+            _print_result(test_description, test_returncode)
+
+    # handle fixture's one time teardown
+    if 'onetime_teardown' in fixture_config:
+        script = _build_commandline(fixture_config['onetime_teardown'])
+        (out, err, returncode) = _execute_commandline(script)
+        _print_verbose(verbose, out, err, returncode)
+        _terminate_if_required(returncode)
+
+    return 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', dest='filename', default='Testfile')
+    parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true')
+    
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.filename):
+        print('Testfile not found' + os.linesep, file=sys.stderr)
+        parser.print_help()
+        sys.exit(1)
+
+    stream = file(args.filename, 'r')
+    config = yaml.load(stream) # Loader=yaml.Loader / yaml.SafeLoader
+
+    returncode = execute_testfile(config, args.verbose)
+    sys.exit(returncode)
+
---
+++ unittests.py
@@ -0,0 +1,311 @@
+import mock
+import unittest
+
+import testfile
+
+class Testfile_tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_single_test__no_testcases__executes_ok(self):
+        config = {'tests': {}}
+        returncode = testfile.execute_testfile(config)
+        self.assertEqual(0, returncode)
+
+    @mock.patch('testfile._build_commandline')
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__single_testcase__executes_ok(self, mock_term, mock_result, mock_verbose, mock_execute, mock_build):
+        config = {
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            }]
+        }
+        returncode = testfile.execute_testfile(config)
+        self.assertEqual(0, returncode)
+
+    @mock.patch('testfile._build_commandline')
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__multiple_testcases__executes_ok(self, mock_term, mock_result, mock_verbose, mock_execute, mock_build):
+        config = {
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            },
+            {
+                'test': 'test_2',
+                'steps': [
+                    'echo bar'
+                ]
+            },
+            ]
+        }
+        returncode = testfile.execute_testfile(config)
+        self.assertEqual(0, returncode)
+
+    @mock.patch('testfile._build_commandline')
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__no_fixture__ok(self, mock_term, mock_result, mock_verbose, mock_execute, mock_build):
+        config = {
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            }
+            ]
+        }
+        returncode = testfile.execute_testfile(config)
+        self.assertEqual(0, returncode)
+
+    @mock.patch('testfile._build_commandline')
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__no_tests__should_not_break(self, mock_term, mock_result, mock_verbose, mock_execute, mock_build):
+        config = {
+        }
+        returncode = testfile.execute_testfile(config)
+        self.assertEqual(0, returncode)
+
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__one_time_setup__called_exactly_once(self, mock_term, mock_result, mock_verbose, mock_execute):
+        config = {
+            'fixture': {
+                'onetime_setup': 'foo',
+            },
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            },
+            {
+                'test': 'test_2',
+                'steps': [
+                    'echo bar'
+                ]
+            },
+            ]
+        }
+        testfile.execute_testfile(config, verbose=True)
+        self.assertEqual(3, mock_execute.call_count)
+        mock_execute.assert_has_calls([mock.call('foo'),
+                                       mock.call('echo foo'),
+                                       mock.call('echo bar')])
+
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__one_time_teardown__called_exactly_once(self, mock_term, mock_result, mock_verbose, mock_execute):
+        config = {
+            'fixture': {
+                'onetime_teardown': 'foo'
+            },
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            },
+            {
+                'test': 'test_2',
+                'steps': [
+                    'echo bar'
+                ]
+            },
+            ]
+        }
+        testfile.execute_testfile(config, verbose=True)
+        self.assertEqual(3, mock_execute.call_count)
+        mock_execute.assert_has_calls([mock.call('echo foo'),
+                                       mock.call('echo bar'),
+                                       mock.call('foo')])
+
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__teardown_called_for_each_test(self, mock_term, mock_result, mock_verbose, mock_execute):
+        config = {
+            'fixture': {
+                'teardown': 'foo'
+            },
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            },
+            {
+                'test': 'test_2',
+                'steps': [
+                    'echo bar'
+                ]
+            },
+            ]
+        }
+        testfile.execute_testfile(config, verbose=True)
+        self.assertEqual(4, mock_execute.call_count)
+        mock_execute.assert_has_calls([mock.call('echo foo'),
+                                       mock.call('foo'),
+                                       mock.call('echo bar'),
+                                       mock.call('foo')])
+
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__setup_called_for_each_test(self, mock_term, mock_result, mock_verbose, mock_execute):
+        config = {
+            'fixture': {
+                'setup': 'foo'
+            },
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            },
+            {
+                'test': 'test_2',
+                'steps': [
+                    'echo bar'
+                ]
+            },
+            ]
+        }
+        testfile.execute_testfile(config, verbose=True)
+        self.assertEqual(4, mock_execute.call_count)
+        mock_execute.assert_has_calls([mock.call('foo'),
+                                       mock.call('echo foo'),
+                                       mock.call('foo'),
+                                       mock.call('echo bar')])
+
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__optional_description(self, mock_term, mock_result, mock_verbose, mock_execute):
+        config = {
+            'tests': [
+            {
+                'test': 'test_1',
+                'description': 'foobarbaz',
+                'steps': [
+                    'echo foo'
+                ]
+            },
+            ]
+        }
+        testfile.execute_testfile(config, verbose=True)
+        mock_result.assert_has_calls([mock.call('test_1 (foobarbaz)', 0)])
+
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 3))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__test_fails__print_error(self, mock_term, mock_result, mock_verbose, mock_execute):
+        config = {
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            },
+            ]
+        }
+        testfile.execute_testfile(config, verbose=True)
+        mock_result.assert_has_calls([mock.call('test_1', 3)])
+
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__test_succeeds__print_pass(self, mock_term, mock_result, mock_verbose, mock_execute):
+        config = {
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            },
+            ]
+        }
+        testfile.execute_testfile(config, verbose=True)
+        mock_result.assert_has_calls([mock.call('test_1', 0)])
+
+    @mock.patch('testfile._execute_commandline', side_effect=[('', '', 0), ('', '', 3)])
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__multiple_tests_one_fails__print_correct_error(self, mock_term, mock_result, mock_verbose, mock_execute):
+        config = {
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo'
+                ]
+            },
+            {
+                'test': 'test_2',
+                'steps': [
+                    'echo bar'
+                ]
+            },
+            ]
+        }
+        testfile.execute_testfile(config, verbose=True)
+        mock_result.assert_has_calls([mock.call('test_1', 0), mock.call('test_2', 3)])
+
+    @mock.patch('testfile._execute_commandline', return_value=('', '', 0))
+    @mock.patch('testfile._print_verbose')
+    @mock.patch('testfile._print_result')
+    @mock.patch('testfile._terminate_if_required')
+    def test_single_test__test_multiple_steps__print_pass(self, mock_term, mock_result, mock_verbose, mock_execute):
+        config = {
+            'tests': [
+            {
+                'test': 'test_1',
+                'steps': [
+                    'echo foo',
+                    'echo bar',
+                    'echo baz'
+                ]
+            },
+            ]
+        }
+        testfile.execute_testfile(config, verbose=True)
+        mock_execute.assert_has_calls([mock.call('echo foo;echo bar;echo baz')])
+
+if __name__ == '__main__':
+    unittest.main()
+
